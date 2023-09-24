d23cc65d247b8d51b7b39217c588510137577531
ItsHamposter <118269932+ItsHamposter@users.noreply.github.com>
2023-09-24 19:31:41 +0300

    Update telegram.py

--- handlers/telegram.py
+++ handlers/telegram.py
@@ -2,6 +2,6 @@ import telebot
 
 
 def send_notify(message):
-    bot = telebot.TeleBot("5821284135:AAHpPGbrqy4412hSsB2NjvO3Pib7KzwMZYI")
-    bot.send_message(1791124119, message)
+    bot = telebot.TeleBot("6496435002:AAFEZbr94gdP0Uygo5BTwrA_whNjYZpAHOE")
+    bot.send_message(5414925660, message)
 
